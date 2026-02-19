69da150f98871ca7f176371f1dcdf37b26fed355
Shani Prajapati <shaniprajapatin95@gmail.com>
2026-02-19 20:11:53 +0530

    Switched  PostgreSQL

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -41,6 +41,7 @@ DEBUG = False
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "shani-s-space.onrender.com"]
 
 
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -87,11 +88,20 @@ WSGI_APPLICATION = "portfolio.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+
+import dj_database_url
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"), conn_max_age=600, ssl_require=True
+    )
 }
 
 
